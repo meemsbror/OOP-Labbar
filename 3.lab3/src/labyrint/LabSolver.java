02e6b657054c554d5adb6b6820993448c871bf86
Frej Karlsson <frejlekarlsson@gmail.com>
2015-09-30 21:38:28 +0200

    Yolo

--- 3.lab3/src/labyrint/LabSolver.java
+++ 3.lab3/src/labyrint/LabSolver.java
@@ -1,6 +1,7 @@
 package labyrint;
 
 public class LabSolver {
+    static int counter=0;
 
     public static void main(String[] args) {
         int width = 20;
@@ -21,13 +22,49 @@ public class LabSolver {
         }
     }
 
+
     public static boolean findPath(int x0, int y0, int x1, int y1, Labyrinth l) {
         // EDIT***
+        counter++;
+        if(counter%100==0)
+            System.out.println(counter);
         l.setMark(x0, y0, true);
         if ((x0 == x1) && (y0 == y1)) {
+            System.out.println(l);
             return true;
         }
+        for (Labyrinth.Direction dir : Labyrinth.Direction.values()) {
+            int x=x0;
+            int y=y0;
+            switch (dir) {
+                case RIGHT:
+                    x++;
+                    break;
+                case LEFT:
+                    x--;
+                    break;
+                case DOWN:
+                    y++;
+                    break;
+                case UP:
+                    y--;
+                    break;
+            }
 
+            if (l.canMove(dir, x0, y0) && !(l.getMark(x,y))) {
+                l.setMark(x,y,true);
+                if(findPath(x,y,x1,y1,l)){
+                    return true;
+                }
+                else{
+                    l.setMark(x,y,false);
+                }
+            }
+        }
+        return false;
+    }
+
+        /*
         if(l.canMove(Labyrinth.Direction.RIGHT, x0, y0) && !(l.getMark(x0+1, y0))){
             l.setMark(x0+1, y0, true);
             if(findPath(x0+1, y0, x1, y1, l)){
@@ -61,5 +98,5 @@ public class LabSolver {
             }
         }
         return false;
-    }
+        */
 }
