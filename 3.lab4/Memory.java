57431d44432de2477fd507546ed9ca51f69d3299
Frej Karlsson <frejlekarlsson@gmail.com>
2015-10-27 16:25:10 +0100

    löste kritiken från labben

--- 3.lab4/Memory.java
+++ 3.lab4/Memory.java
@@ -7,7 +7,9 @@ import java.util.Random;
 
 public class Memory extends JFrame{
 
-    public int width, height, playerTurn, pictureCount, score[];
+    private boolean sameCard=false;
+
+    private int width, height, playerTurn, pictureCount, score[];
 
     //Alla spelare
     public Person [] players;
@@ -25,15 +27,89 @@ public class Memory extends JFrame{
 
 
     public Memory(){
+
         timerStarted=false;
         int players;
 
+        players=userInput();
+        createBoard(players);
+
         /*
-        Tar indata och kollar om datan är giltig. Om den inte är det
-        skriver den ut vad som är fel och ber om giltig indata.
+        Skapar ett nytt spel. Återställer vissa värden. Till exempel:
+        Vilka bilder som visas, poängen spelarna har och lägger ut de nya korten
+        (ändrar allas kort status till DOLT)
          */
+        nyttSpel();
+
+
+        }
+
+    //Skapar brädet.
+    public void createBoard(int players){
+        /*
+        Initierar arrayer med kort och spelare med rätt längd.
+        */
+        pictureCount = width * height;
+        this.score = new int [players];
+        this.kort = new Kort [pictureCount];
+        this.players = new Person[players];
+
+        main = new JPanel(new BorderLayout());
+        playPanel = new JPanel(new GridLayout(players, 1));
+        optionPanel = new JPanel(new FlowLayout());
+        gamePanel = new JPanel(new GridLayout(height,width));
+
+        /*
+        Lägger till spelare i players arrayen.
+        */
+        for(int i = 0; i < players; i++){
+            this.players[i]=new Person(i + 1);
+            playPanel.add(this.players[i]);
+        }
+
+        /*
+        Byter färg på spelaren vars tur det är, altså spelare 1 just nu.
+         */
+        this.players[playerTurn].changeTurn(true);
+
+        /*
+        Skapar 2 knappar (new och quit) samt ger dem actionListener och actionCommand.
+         */
+        createButtons();
+
+
+        //Lägger till
+        main.add(playPanel, BorderLayout.WEST);
+        main.add(optionPanel, BorderLayout.SOUTH);
+        main.add(gamePanel, BorderLayout.CENTER);
+
+        add(main);
+
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setLocation(50,50);
+        setSize(width * 150 + 100, height * 150);
+        setResizable(false);
+        setVisible(true);
+
+    }
+    public void createButtons(){
+        Buttons buttons = new Buttons();
+        JButton option1 = new JButton("NEW");
+        JButton option2 = new JButton("QUIT");
+        option1.setActionCommand("new");
+        option1.addActionListener(buttons);
+        option2.setActionCommand("exit");
+        option2.addActionListener(buttons);
+        optionPanel.add(option1);
+        optionPanel.add(option2);
+    }
+    /*
+    Tar indata och kollar om datan är giltig. Om den inte är det
+    skriver den ut vad som är fel och ber om giltig indata.
+    */
+    public int userInput(){
+        int players;
         while(true) {
-                //Lägg till exceptions
             String input = JOptionPane.showInputDialog("Bredd?");
 
             if(!isNumerical(input)) {
@@ -53,11 +129,9 @@ public class Memory extends JFrame{
                 continue;
             }
             players = Integer.valueOf(input);
-            if(!(players>9)){
 
-            }
             if(((height*width) % 2 == 0 && height*width <= 36) && (players<10)){
-                break;
+                return players;
             }else if(!((height*width) % 2 == 0 )){
                 JOptionPane.showMessageDialog(null, "Inte jämnt antal ju..");
             }else if(!(height*width <= 36)){
@@ -66,68 +140,7 @@ public class Memory extends JFrame{
                 JOptionPane.showMessageDialog(null, "Lite många spelare där eller..?");
             }
         }
-        pictureCount = width * height;
-        /*
-        Initierar arrayer med kort och spelare med rätt längd.
-         */
-        this.score = new int [players];
-        this.kort = new Kort [pictureCount];
-        this.players = new Person[players];
-
-        main = new JPanel(new BorderLayout());
-        playPanel = new JPanel(new GridLayout(players, 1));
-        optionPanel = new JPanel(new FlowLayout());
-        gamePanel = new JPanel(new GridLayout(height,width));
-
-        /*
-        Skapar 2 knappar (new och quit) samt ger dem actionListener och actionCommand.
-         */
-
-        Buttons buttons = new Buttons();
-        JButton option1 = new JButton("NEW");
-        JButton option2 = new JButton("QUIT");
-        option1.setActionCommand("new");
-        option1.addActionListener(buttons);
-        option2.setActionCommand("exit");
-        option2.addActionListener(buttons);
-
-
-        /*
-        Lägger till spelare i players arrayen.
-         */
-        for(int i = 0; i < players; i++){
-            this.players[i]=new Person(i + 1);
-            playPanel.add(this.players[i]);
-        }
-
-        /*
-        Byter färg på spelaren vars tur det är, altså spelare 1 just nu.
-         */
-        this.players[playerTurn].changeTurn(true);
-
-        /*
-        Skapar ett nytt spel. Återställer vissa värden. Till exempel:
-        Vilka bilder som visas, poängen spelarna har och lägger ut de nya korten (ändrar allas kort status till DOLT)
-         */
-        nyttSpel();
-
-        optionPanel.add(option1);
-        optionPanel.add(option2);
-
-        main.add(playPanel, BorderLayout.WEST);
-        main.add(optionPanel, BorderLayout.SOUTH);
-        main.add(gamePanel, BorderLayout.CENTER);
-
-        add(main);
-
-        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        setLocation(50,50);
-        setSize(width * 150 + 100, height * 150);
-        setVisible(true);
-        setResizable(false);
-        }
-
-
+    }
     //Denna metod säger om en sträng endast innehåller siffror.
     public boolean isNumerical(String temp) {
         String allNumbers = "1234567890";
@@ -246,67 +259,18 @@ public class Memory extends JFrame{
     */
     private class Listener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
-            if(e.getSource().getClass().toString().equals("class Kort")&&!timerStarted){
-
+            if((e.getSource() instanceof Kort) && !timerStarted){
                 Kort k = (Kort)e.getSource();
                 if(k!=activeKort && k.getStatus() != Kort.Status.SAKNAS) {
                     if (kortShowing) {
                         //Hanterar i fall de två kort som visas är likadana och kollar även ifall någon har vunnit.
+                        timer.restart();
+                        timer.start();
+                        timerStarted = true;
                         if (k.sammaBild(activeKort)) {
-                            k.setStatus(Kort.Status.SAKNAS);
-                            activeKort.setStatus(Kort.Status.SAKNAS);
-                            kortShowing = false;
-                            score[playerTurn]++;
-                            players[playerTurn].updateScore(playerTurn+1);
-                            boolean temp = false;
-                            for(Kort kot : kort){
-                                if(!(kot.getStatus() == Kort.Status.SAKNAS)) {
-                                    temp = true;
-                                    break;
-                                }
-                            }
-                            if (!temp) {
-                                int tempInt = 0;
-                                String winners="";
-                                String [] winnersArr;
-                                for(int i = 0; score.length-1 > i; i++){
-                                    if(score[i]>tempInt) {
-                                        tempInt = score[i];
-                                    }
-                                }
-                                for(int i = 0; score.length > i; i++){
-                                    if(score[i]==tempInt){
-                                        winners+=(i+1)+" ";
-                                    }
-                                }
-                                winners.trim();
-                                winnersArr=winners.split(" ");
-                                if(winnersArr.length>1){
-                                    winners="";
-                                    for(int i=0;i<winnersArr.length-2;i++){
-                                        winners+=winnersArr[i]+",";
-                                    }
-                                    winners+= winnersArr[winnersArr.length-2]+ " och " +winnersArr[winnersArr.length-1]+" fick lika mycket poäng";
-                                }else{
-                                    winners+="vann";
-                                }
-
-                                int dialogButton = JOptionPane.showConfirmDialog(null, "Spelare nummer "
-                                                                                    + (winners)
-                                                                                    + "\nVill ni spela igen? ;)",
-                                                                                    "Hejja",
-                                                                                    JOptionPane.YES_NO_OPTION);
-                                if(dialogButton == JOptionPane.YES_OPTION) {
-                                    nyttSpel();
-                                }else{
-                                    System.exit(3);
-                                }
-                            }
+                            sameCard=true;
                         }
                         else {
-                            timer.restart();
-                            timer.start();
-                            timerStarted = true;
                             players[playerTurn].changeTurn(false);
                             playerTurn=(playerTurn+1)%players.length;
                             players[playerTurn].changeTurn(true);
@@ -322,13 +286,69 @@ public class Memory extends JFrame{
                 }
 
             //Hanterar Timerns actionevent
-            }else if (e.getSource().getClass().toString().equals("class javax.swing.Timer")){
+            }else if (e.getSource() instanceof Timer){
+                if(sameCard){
+                    for(Kort k:kort){
+                        if(k.getStatus()==Kort.Status.SYNLIGT){
+                            k.setStatus(Kort.Status.SAKNAS);
+                        }
+                    }
+                    kortShowing = false;
+                    score[playerTurn]++;
+                    players[playerTurn].updateScore(playerTurn+1);
+                    boolean temp = false;
+                    for(Kort kot : kort){
+                        if(!(kot.getStatus() == Kort.Status.SAKNAS)) {
+                            temp = true;
+                            break;
+                        }
+                    }
+                    if (!temp) {
+                        int tempInt = 0;
+                        String winners="";
+                        String [] winnersArr;
+                        for(int i = 0; score.length-1 > i; i++){
+                            if(score[i]>tempInt) {
+                                tempInt = score[i];
+                            }
+                        }
+                        for(int i = 0; score.length > i; i++){
+                            if(score[i]==tempInt){
+                                winners+=(i+1)+" ";
+                            }
+                        }
+                        winners.trim();
+                        winnersArr=winners.split(" ");
+                        if(winnersArr.length>1){
+                            winners="";
+                            for(int i=0;i<winnersArr.length-2;i++){
+                                winners+=winnersArr[i]+",";
+                            }
+                            winners+= winnersArr[winnersArr.length-2]+ " och " +winnersArr[winnersArr.length-1]+" fick lika mycket poäng";
+                        }else{
+                            winners+="vann";
+                        }
+
+                        int dialogButton = JOptionPane.showConfirmDialog(null, "Spelare nummer "
+                                        + (winners)
+                                        + "\nVill ni spela igen? ;)",
+                                "Hejja",
+                                JOptionPane.YES_NO_OPTION);
+                        if(dialogButton == JOptionPane.YES_OPTION) {
+                            nyttSpel();
+                        }else{
+                            System.exit(3);
+                        }
+                    }
+                }
+
                 for(Kort ko : kort) {
                     if(ko.getStatus() == Kort.Status.SYNLIGT) {
                         ko.setStatus(Kort.Status.DOLT);
 
                     }
                 }
+                sameCard=false;
                 timer.stop();
                 timerStarted=false;
                 kortShowing=false;
